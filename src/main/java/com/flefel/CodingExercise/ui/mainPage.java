bfc431e5053dd1163b8bed4869fb3ce52f614bf7
Mohamad Flefel <mflefel@hotmail.com>
2018-02-27 22:41:28 +0200

    add vaadin mainpage

---
+++ src/main/java/com/flefel/CodingExercise/ui/mainPage.java
@@ -0,0 +1,15 @@
+package com.flefel.CodingExercise.ui;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+public class mainPage extends UI {
+    @Override
+    public void init(VaadinRequest request) {
+        VerticalLayout layout = new VerticalLayout();
+        setContent(layout);
+        layout.addComponent(new Label("Hello, world!"));
+    }
+}
