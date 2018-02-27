746d2f6acb5bd5e107975f6488424f3d59dc40b7
Mohamad Flefel <mflefel@hotmail.com>
2018-02-27 23:37:32 +0200

    vaadin

--- src/main/java/com/flefel/CodingExercise/ui/mainPage.java
+++ src/main/java/com/flefel/CodingExercise/ui/mainPage.java
@@ -1,9 +1,13 @@
 package com.flefel.CodingExercise.ui;
 
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+
+import javax.servlet.annotation.WebServlet;
 
 public class mainPage extends UI {
     @Override
@@ -11,5 +15,9 @@ public class mainPage extends UI {
         VerticalLayout layout = new VerticalLayout();
         setContent(layout);
         layout.addComponent(new Label("Hello, world!"));
+    }
+    @WebServlet(urlPatterns = "/*")
+    @VaadinServletConfiguration(ui = mainPage.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
     }
 }
